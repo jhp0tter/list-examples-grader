dcd903cd44ce6b9388d31c87d5e6f11a91d97657
Jeremy Potter <jhpotter@protonmail.com>
2022-11-10 10:58:44 -0800

    Write grading script

--- grade.sh
+++ grade.sh
@@ -1,7 +1,31 @@
-# Create your grading script here
+#!/bin/sh
 
-set -e
+err() {
+  echo $1
+  exit 1
+}
 
-rm -rf student-submission
-git clone $1 student-submission
+DIR="subm"
+CP=".\
+:lib/hamcrest-core-1.3.jar\
+:lib/junit-4.13.2.jar"
 
+test ! -z "$1" \
+|| err "PLEASE SUPPLY A GIT REPOSITORY"
+
+rm -rf "${DIR}"
+git clone $1 "${DIR}"
+
+test -e "${DIR}/ListExamples.java" \
+|| err "FILE NOT FOUND: ListExamples.java"
+
+cp TestListExamples.java "${DIR}"
+cp -r lib "${DIR}"
+cd "${DIR}"
+
+javac -cp "${CP}" *.java \
+|| err "COMPILATION FAILED"
+java -cp "${CP}" org.junit.runner.JUnitCore TestListExamples \
+> ../report.txt
+
+echo "REPORT SAVED TO: report.txt"
