ce0d2fb2cabc3c85e243364a17455ef5eb46f78b
yukthadeesan <114612660+yukthadeesan@users.noreply.github.com>
2022-11-17 12:21:02 -0800

    Update TestListExamples.java

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,8 +1,24 @@
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.*;
 
+class greaterThan3 implements StringChecker{
+    public boolean checkString(String s) {
+      return s.length()>3;
+    }
+}
+
+class lesserThan5 implements StringChecker{
+  public boolean checkString(String s) {
+    return s.length()<5;
+  } 
+}
+
 public class TestListExamples {
-    // Write your grading tests here!
+  // Write your grading tests here!
 
   @Test 
   public void test1(){
@@ -23,3 +39,24 @@ public class TestListExamples {
     List actual= ListExamples.filter(testlist1, st);
     assertEquals(expected, actual);
   }
+
+  @Test 
+  public void test2(){
+    List testlist2= new ArrayList<>(); 
+    testlist2.add("music");
+    testlist2.add("for");
+    testlist2.add("a");
+    testlist2.add("sushi");
+    testlist2.add("restaurant");
+
+    StringChecker st = new greaterThan5();
+
+    List expected= new ArrayList<>();
+    expected.add("for");
+    expected.add("a");
+    
+    
+    List actual= ListExamples.filter(testlist2, st);
+    assertEquals(expected, actual);
+  }
+}
